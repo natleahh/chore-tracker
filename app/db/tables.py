41f86605b93fdb364899390896d2459aad24e6fc
natleahh <a.l.tengbe@gmail.com>
2026-01-19 20:48:09 +0000

    fix: make Chore table a table

--- app/db/tables.py
+++ app/db/tables.py
@@ -5,7 +5,7 @@ from sqlmodel import Field
 from app.models import core
 
 
-class Chore(core.Chore):
+class Chore(core.Chore, table=True):
     """Chore database table."""
 
     id: int | None = Field(default=None, primary_key=True)
