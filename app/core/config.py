618f5e7bf71c430698cdb8acb2c284dee98fa927
natleahh <a.l.tengbe@gmail.com>
2026-01-19 18:10:34 +0000

    feat: add core modules

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+_ = load_dotenv()
+
+
+class Config(BaseSettings):
+    db_name: str = "test.db"
+    db_path: str = "./tmp"
+
+    @property
+    def db_url(self):
+        return f"sqlite:///{self.db_path}/{self.db_name}"
+
+
+config = Config()
